99649c6a34167256942f9f27116b7d969030aa77
Kenneth Seals-Nutt <kenneth@seals-nutt.com>
2020-05-08 13:39:51 -0700

    (v0.0.12) DOC: Adding version bump to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='django-wikidata-api',
-    version='0.0.11',
+    version='0.0.12',
     packages=['django_wikidata_api'],
     description='Python Package for interfacing with Wikidata within a Django App',
     long_description=README,
